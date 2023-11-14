377db65b83f0465d44180dadf9d98d2e3a5fe27d
Santiago Inostroza <santiagoinostroza2@gmail.com>
2023-11-14 01:06:35 -0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    manifest: {
+      "name": "Tetris Game",
+      "short_name": "Tetris",
+      "start_url": "/tetris-game/",
+      "display": "standalone",
+      "background_color": "#000000",
+      "description": "Tetris Game",
+      "icons": [
+        {
+          "src": "/tetris-game/assets/icons/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "/tetris-game/assets/icons/android-chrome-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "/tetris-game/assets/icons/android-chrome-maskable-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "/tetris-game/assets/icons/android-chrome-maskable-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    }
+  })],
   base: '/tetris-game/'
 })
 
