c15f480c7cb38ae7e500f8017583074c45d74cb1
Santiago Inostroza <santiagoinostroza2@gmail.com>
2023-11-22 16:13:26 -0300

    se consulta api de geolocalizacion para obtner datos de pais

--- src/api/apiScore.js
+++ src/api/apiScore.js
@@ -7,6 +7,8 @@ const API_URL_PRODUCCION = 'https://tetrisbackend.saig.cl/api/';
 let url = null;
 let api_url = null;
 
+
+
 if(ISPRODUCTION){
     api_url = API_URL_PRODUCCION;
 }
@@ -14,8 +16,13 @@ else{
     api_url = API_URL_LOCAL;
 }
 
+const API_KEY_IPGEOLOCATION = "33ce76efdd2c425fba086db0e27acc5e";
+const URL_IPGEOLOCATION = "https://api.ipgeolocation.io/ipgeo?apiKey=" + API_KEY_IPGEOLOCATION;
+
+
+
 const getDataUser = async () => {
-   const response = await fetch('https://ip-api.com/json');
+   const response = await fetch(URL_IPGEOLOCATION);
   return await response.json();
 };
 
@@ -38,10 +45,12 @@ export async function createPlayer(player) {
     let url = `${api_url}players`;
     try {
         let data = await getDataUser();
-        player.country = data.country;
+        player.country = data.country_name;
         player.city = data.city;
-        player.latitude = data.lat;
-        player.longitude = data.lon;
+        player.latitude = data.latitude;
+        player.longitude = data.longitude;
+        player.country_flag = data.country_flag;
+        console.log('2', player);
         return await axios.post(url, player);
     } catch (error) {
         console.error(error);
